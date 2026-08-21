60a94f89ee7e045257603852e3fb24e34d58bcd5
Joy Diganta <joydiganta.bd@gmail.com>
2026-08-21 22:31:56 +0600

    Reduce font size of main headings

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ st.markdown(
 
             /* Main headings */
             h1 {
-                font-size: 1.4rem !important;
+                font-size: 1.25rem !important;
                 line-height: 1.08 !important;
                 word-break: normal !important;
             }
